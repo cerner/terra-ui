b265cf04e87bbea1c322553d69989646716389fc
Matt Butler <mattwbutler@gmail.com>
2016-10-18 15:10:14 -0500

    Config travis ci & disabled stylelint rules (#4)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'terra_label/index'
+
   root 'terra#index'
 
   match 'terra', to: 'terra#index', via: :get
   match 'terra-application/', to: 'terra_application#index', via: :get
   match 'terra-button', to: 'terra_button#index', via: :get
   match 'terra-grid', to: 'terra_grid#index', via: :get
+  match 'terra-label', to: 'terra_label#index', via: :get
   match 'terra-legacy-theme', to: 'terra_legacy_theme#index', via: :get
   match 'terra-menu', to: 'terra_menu#index', via: :get
   match 'terra-table', to: 'terra_table#index', via: :get

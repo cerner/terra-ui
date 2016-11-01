ab7a622cc5de137fc6baa3cf09f2c2c1ee1e5b16
Matt Butler <mattwbutler@gmail.com>
2016-11-01 14:29:15 -0500

    adding image component

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'terra_label/index'
-
   root 'terra#index'
 
   match 'terra', to: 'terra#index', via: :get
   match 'terra-application/', to: 'terra_application#index', via: :get
   match 'terra-button', to: 'terra_button#index', via: :get
   match 'terra-grid', to: 'terra_grid#index', via: :get
+  match 'terra-image', to: 'terra_image#index', via: :get
   match 'terra-label', to: 'terra_label#index', via: :get
   match 'terra-legacy-theme', to: 'terra_legacy_theme#index', via: :get
   match 'terra-menu', to: 'terra_menu#index', via: :get

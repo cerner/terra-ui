5275f5dd8b39cde4e03901fbf96336d1420674a5
Matt Butler <mattwbutler@gmail.com>
2017-07-14 09:53:37 -0500

    getting-started-toc

--- client/app/bundles/App/GettingStarted/Index.jsx
+++ client/app/bundles/App/GettingStarted/Index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router';
 /* eslint-disable import/no-extraneous-dependencies */
 import Markdown from 'terra-markdown';
 import Grid from 'terra-grid';
+import List from 'terra-list';
+import Heading from 'terra-heading';
 /* eslint-enable import/no-extraneous-dependencies */
 import WhatIsTerra from './WhatIsTerra.md';
 import Features from './Features.md';
@@ -12,26 +15,42 @@ import ConfiguringWebpack from './ConfiguringWebpack.md';
 import I18n from './I18n.md';
 import Directionality from './Directionality.md';
 import Theming from './Theming.md';
+import styles from './Index.scss';
 
+
+const scrollToComponent = (id) => {
+  document.querySelector(id).scrollIntoView();
+};
 
 const GettingStartedPage = () => (
   <div>
     <Grid>
       <Grid.Row>
-        <Grid.Column col={1} />
-        <Grid.Column col={10} >
-          <br />
-          <Markdown id="what-is-terra" src={WhatIsTerra} />
-          <Markdown id="features" src={Features} />
-          <Markdown id="installing-components" src={InstallingComponents} />
-          <Markdown id="dependencies" src={Dependencies} />
-          <Markdown id="basic-usage" src={BasicUsage} />
-          <Markdown id="configuring-webpack" src={ConfiguringWebpack} />
-          <Markdown id="i18n" src={I18n} />
-          <Markdown id="directionality" src={Directionality} />
-          <Markdown id="theming" src={Theming} />
+        <Grid.Column small={2} className={styles['components-vertical-nav']}>
+          <List className={styles['vertical-menu']}>
+            <List.Item content={<Heading level={2}>Getting Started</Heading>} />
+            <List.Item content={<Link to="/getting-started#what-is-terra-" onClick={() => { scrollToComponent('#what-is-terra-'); }} >What is Terra?</Link>} />
+            <List.Item content={<Link to="/getting-started#features" onClick={() => { scrollToComponent('#features'); }} >Features</Link>} />
+            <List.Item content={<Link to="/getting-started#installing-components" onClick={() => { scrollToComponent('#installing-components'); }} >Installing Components</Link>} />
+            <List.Item content={<Link to="/getting-started#dependencies" onClick={() => { scrollToComponent('#dependencies'); }} >Dependencies</Link>} />
+            <List.Item content={<Link to="/getting-started#basic-usage" onClick={() => { scrollToComponent('#basic-usage'); }} >Basic Usage</Link>} />
+            <List.Item content={<Link to="/getting-started#configuring-webpack" onClick={() => { scrollToComponent('#configuring-webpack'); }} >Configuring Webpack</Link>} />
+            <List.Item content={<Link to="/getting-started#internationalization-i18n-and-localization-l10n-" onClick={() => { scrollToComponent('#internationalization-i18n-and-localization-l10n-'); }} >I18n</Link>} />
+            <List.Item content={<Link to="/getting-started#theming" onClick={() => { scrollToComponent('#theming'); }} >Theming</Link>} />
+          </List>
         </Grid.Column>
-        <Grid.Column col={1} />
+        <Grid.Column small={10} >
+          <br />
+          <Markdown src={WhatIsTerra} />
+          <Markdown src={Features} />
+          <Markdown src={InstallingComponents} />
+          <Markdown src={Dependencies} />
+          <Markdown src={BasicUsage} />
+          <Markdown src={ConfiguringWebpack} />
+          <Markdown src={I18n} />
+          <Markdown src={Directionality} />
+          <Markdown src={Theming} />
+        </Grid.Column>
       </Grid.Row>
     </Grid>
   </div>

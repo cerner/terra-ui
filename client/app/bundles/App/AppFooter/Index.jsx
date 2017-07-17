ac03cad2fecce3f8843a2c8370b2181d47d79d50
Brett Jankord <brett.jankord@cerner.com>
2017-07-17 08:54:28 -0500

    Update footer section titles

--- client/app/bundles/App/AppFooter/Index.jsx
+++ client/app/bundles/App/AppFooter/Index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 /* eslint-disable import/no-extraneous-dependencies */
 import Grid from 'terra-grid';
-import Heading from 'terra-heading';
+import Text from 'terra-text';
 import List from 'terra-list';
 /* eslint-enable import/no-extraneous-dependencies */
 import cernerLogo from './cerner_logo.png';
@@ -13,7 +13,7 @@ const AppFooter = () => (
       <Grid.Row>
         <Grid.Column col={1} />
         <Grid.Column className={styles['footer-col']} small={2} >
-          <Heading level={3} size="small" color={'#a6d9f4'}>GitHub</Heading>
+          <Text fontSize="18" weight="700" color={'#a6d9f4'}>GitHub</Text>
           <List>
             <List.Item content={<a href="https://github.com/cerner/terra-core">terra-core</a>} />
             <List.Item content={<a href="https://github.com/cerner/terra-clinical">terra-clinical</a>} />
@@ -21,13 +21,13 @@ const AppFooter = () => (
           </List>
         </Grid.Column>
         <Grid.Column className={styles['footer-col']} small={2}>
-          <Heading level={3} size="small" color={'#a6d9f4'}>Support</Heading>
+          <Text fontSize="18" weight="700" color={'#a6d9f4'}>Support</Text>
           <List>
             <List.Item content={<a href="https://github.com/cerner/terra-core/issues/new">Log an issue</a>} />
           </List>
         </Grid.Column>
         <Grid.Column className={styles['footer-col']} small={2}>
-          <Heading level={3} size="small" color={'#a6d9f4'}>About Us</Heading>
+          <Text fontSize="18" weight="700" color={'#a6d9f4'}>About Us</Text>
           <List>
             <List.Item content={<a href="https://www.cerner.com/">Cerner</a>} />
             <List.Item content={<a href="http://engineering.cerner.com/">Engineering</a>} />

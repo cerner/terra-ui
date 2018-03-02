b754c585e561941a788bfd2efd2cf291a0dd18f2
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2018-03-02 14:38:16 -0600

    Lets use i18n plugin. This commit add i18nAggregated translations for proof of concept.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,13 @@
 const webpackConfig = require('terra-dev-site/src/config/webpack.config');
+const I18nAggregatorPlugin = require('terra-i18n-plugin');
+const i18nSupportedLocales = require('terra-i18n/lib/i18nSupportedLocales');
+const path = require('path');
+const glob = require('glob');
+
+
+const searchPattern = path.resolve(process.cwd(), 'node_modules', 'terra-*', 'packages', '*');
+
+const translationsDirectories = glob.sync(searchPattern);
 
 const babelLoaderRule = {
   test: /\.(jsx|js)$/,
@@ -14,8 +23,17 @@ moduleRules.unshift(babelLoaderRule);
 
 webpackConfig.module.rules = moduleRules;
 
-// Don't run the i18n plugin.
+
+const i18nAggregator = new I18nAggregatorPlugin({
+  baseDirectory: process.cwd(),
+  translationsDirectories,
+  supportedLocales: i18nSupportedLocales,
+});
+
+// Remove the default default i18n aggregator plugin
 const startPlugins = webpackConfig.plugins.splice(0, 2);
+// Add the terra-ui's i18n aggregator plugin
+startPlugins.push(i18nAggregator);
 const endPlugins = webpackConfig.plugins.splice(1, webpackConfig.plugins.length);
 
 webpackConfig.plugins = startPlugins.concat(endPlugins);

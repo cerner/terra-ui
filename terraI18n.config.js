7547efa6eee9428634154ee019586af01b43ae7b
Matt Henkes <mjhenkes@gmail.com>
2018-04-18 14:01:30 -0500

    fancy up the config

--- terraI18n.config.js
+++ terraI18n.config.js
@@ -1,12 +1,8 @@
 // terraI18n.config.js
-/* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
-const glob = require('glob');
-
-const searchPattern = path.resolve(process.cwd(), 'node_modules', 'terra-*', 'packages', '*', 'translations');
 
 const i18nConfig = {
-  directories: glob.sync(searchPattern),
+  directories: [path.join('node_modules', 'terra-*', 'packages', '*', 'translations')],
 };
 
 module.exports = i18nConfig;

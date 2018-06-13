f42e44780d78ce881d08336c6d09894e862613a4
Nathan Faltermeier <nefalt2@gmail.com>
2018-06-13 16:55:13 -0500

    Consume brand-footer, now to wait for brand-footer release

--- src/Footer/Index.jsx
+++ src/Footer/Index.jsx
@@ -1,45 +1,31 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import Grid from 'terra-grid';
-import Text from 'terra-text';
-import List from 'terra-list';
+import BrandFooter from 'terra-brand-footer';
 /* eslint-enable import/no-extraneous-dependencies */
 import cernerLogo from './cerner_logo.png';
 import styles from './Index.scss';
 
 const AppFooter = () => (
-  <footer className={styles.footer}>
-    <Grid>
-      <Grid.Row>
-        <Grid.Column col={1} />
-        <Grid.Column className={styles['footer-col']} small={2} >
-          <Text fontSize={18} weight={700} color="#a6d9f4">GitHub</Text>
-          <List>
-            <List.Item content={<a href="https://github.com/cerner/terra-clinical">terra-clinical</a>} />
-            <List.Item content={<a href="https://github.com/cerner/terra-core">terra-core</a>} />
-            <List.Item content={<a href="https://github.com/cerner/terra-framework">terra-framework</a>} />
-          </List>
-        </Grid.Column>
-        <Grid.Column className={styles['footer-col']} small={2}>
-          <Text fontSize={18} weight={700} color="#a6d9f4">Support</Text>
-          <List>
-            <List.Item content={<a href="https://github.com/cerner/terra-core/issues/new">Log an issue</a>} />
-          </List>
-        </Grid.Column>
-        <Grid.Column className={styles['footer-col']} small={2}>
-          <Text fontSize={18} weight={700} color="#a6d9f4">About Us</Text>
-          <List>
-            <List.Item content={<a href="https://www.cerner.com/">Cerner</a>} />
-            <List.Item content={<a href="http://engineering.cerner.com/">Engineering</a>} />
-          </List>
-        </Grid.Column>
-        <Grid.Column className={styles['footer-col']} small={4} >
-          <a href="https://www.cerner.com/"><img alt="Cerner Corporation" src={cernerLogo} /></a>
-        </Grid.Column>
-        <Grid.Column col={1} />
-      </Grid.Row>
-    </Grid>
-  </footer>
+  <BrandFooter
+    className={styles.footer}
+    contentBottom={
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <a style={{ display: 'inline-flex' }} href="https://www.cerner.com/">
+          <img alt="Cerner Corporation" src={cernerLogo} />
+        </a>
+      </div>}
+    links={[
+      { text: 'Github' },
+      { text: 'terra-clinical', href: 'https://github.com/cerner/terra-clinical' },
+      { text: 'terra-core', href: 'https://github.com/cerner/terra-core/' },
+      { text: 'terra-framework', href: 'https://github.com/cerner/terra-framework' },
+      { text: 'Support' },
+      { text: 'Log an issue', href: 'https://github.com/cerner/terra-core/issues/new' },
+      { text: 'About Us' },
+      { text: 'Cerner', href: 'https://www.cerner.com/' },
+      { text: 'Engineering', href: 'http://engineering.cerner.com/' },
+    ]}
+  />
 );
 
 export default AppFooter;

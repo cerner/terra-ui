097c4057da91f5c7f3612f2fb899a849a442bf01
Nathan Faltermeier <nefalt2@gmail.com>
2018-07-30 09:58:13 -0500

    Use finalized brand footer props

--- src/Footer/Index.jsx
+++ src/Footer/Index.jsx
@@ -8,22 +8,34 @@ import styles from './Index.scss';
 const AppFooter = () => (
   <BrandFooter
     className={styles.footer}
-    contentBottom={
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <a style={{ display: 'inline-flex' }} href="https://www.cerner.com/">
-          <img alt="Cerner Corporation" src={cernerLogo} />
-        </a>
-      </div>}
-    links={[
-      { text: 'Github' },
-      { text: 'terra-clinical', href: 'https://github.com/cerner/terra-clinical' },
-      { text: 'terra-core', href: 'https://github.com/cerner/terra-core/' },
-      { text: 'terra-framework', href: 'https://github.com/cerner/terra-framework' },
-      { text: 'Support' },
-      { text: 'Log an issue', href: 'https://github.com/cerner/terra-core/issues/new' },
-      { text: 'About Us' },
-      { text: 'Cerner', href: 'https://www.cerner.com/' },
-      { text: 'Engineering', href: 'http://engineering.cerner.com/' },
+    contentLeft={
+      <a href="https://www.cerner.com/"> 
+        <img alt="Cerner Corporation" src={cernerLogo} />
+      </a>
+    }
+    isVertical
+    sections={[
+      {
+        headerText: 'Github',
+        links: [
+          { text: 'terra-clinical', href: 'https://github.com/cerner/terra-clinical' },
+          { text: 'terra-core', href: 'https://github.com/cerner/terra-core/' },
+          { text: 'terra-framework', href: 'https://github.com/cerner/terra-framework' },
+        ],
+      },
+      {
+        headerText: 'Support',
+        links: [
+          { text: 'Log an issue', href: 'https://github.com/cerner/terra-core/issues/new' },
+        ],
+      },
+      {
+        headerText: 'About Us',
+        links: [
+          { text: 'Cerner', href: 'https://www.cerner.com/' },
+          { text: 'Engineering', href: 'http://engineering.cerner.com/' },
+        ],
+      },
     ]}
   />
 );

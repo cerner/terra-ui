e097763b4caa7173b48e5e962aef2e39e70dbff2
Derek Yu <yuderekyu@gmail.com>
2018-10-18 13:35:43 -0500

    doc updates

--- src/terra-dev-site/home/HomeAboutTerra.jsx
+++ src/terra-dev-site/home/HomeAboutTerra.jsx
@@ -11,7 +11,7 @@ const HomeAboutTerra = () => (
       <Grid.Column col={2} />
       <Grid.Column col={8}>
         <Text className={styles['home-about-terra']} fontSize={20} weight={200}>
-          {'Terra UI offers a set of configurable React components designed to help build scalable and modular healthcare focused applications. These components follow Cerner’s Design Standard Guidelines and are well-maintained and tested. The UI framework easily integrates with webpack-based workflows and was created to solve real-world issues in projects we work on day to day.'}
+          {'Terra UI offers a set of configurable React components designed to build scalable and modular healthcare-centric applications. The UI framework was created to solve real world issues in projects we work on, day to day. These components follow Cerner’s Design Standard, are well-maintained and well-tested, and integrate with webpack-based workflows.'}
         </Text>
       </Grid.Column>
       <Grid.Column col={2} />

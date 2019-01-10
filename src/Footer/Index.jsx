682a25f22e10930b0ebe172b557ae5ebcddded90
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2019-01-10 13:51:37 -0600

    update issue url

--- src/Footer/Index.jsx
+++ src/Footer/Index.jsx
@@ -22,7 +22,7 @@ const AppFooter = () => (
       {
         headerText: 'Support',
         links: [
-          { text: 'Log an issue', href: 'https://github.com/cerner/terra-core/issues/new' },
+          { text: 'Log an issue', href: 'https://github.com/cerner/terra-core/issues/new/choose' },
         ],
       },
       {

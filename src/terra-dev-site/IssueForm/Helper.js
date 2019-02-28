f6dd20dac4d2ca14dc0487fa77c01b7f209bc4e6
Adam Parker <amichaelparker@gmail.com>
2019-02-28 12:05:06 -0600

    put error message github link in frustration-free packaging to prevent already entered data from being lost

--- src/terra-dev-site/IssueForm/Helper.js
+++ src/terra-dev-site/IssueForm/Helper.js
@@ -56,7 +56,7 @@ const environmentTemplate = `* Component Name and Version:
 * Webpack Version: 
 * Operating System and version (desktop or mobile): `;
 
-const errorTemplate = packageRepo => `Character count for form exceeded. If you require more space, submit the issue directly to [github](https://github.com/cerner/${packageRepo}/issues/new/choose).`;
+const errorTemplate = packageRepo => `Character count for form exceeded. If you require more space, submit the issue directly to <a href="https://github.com/cerner/${packageRepo}/issues/new/choose" target="_blank">github</a>.`;
 
 const featureTemplate = (description, context, mentions) => `# Feature Request
 

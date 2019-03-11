369e24e97d3f3f799be1fffe6759ec8776848edf
Brett Jankord <bjankord@gmail.com>
2019-03-11 15:28:24 -0500

    Remove reference to design standard (#157)

--- src/terra-dev-site/home/HomeAboutTerra.jsx
+++ src/terra-dev-site/home/HomeAboutTerra.jsx
@@ -9,7 +9,7 @@ const HomeAboutTerra = () => (
       <Grid.Column col={2} />
       <Grid.Column col={8}>
         <Text className={styles['home-about-terra']} fontSize={20} weight={200}>
-          {'Terra UI offers a set of configurable React components designed to build scalable and modular healthcare-centric applications. The UI framework was created to solve real world issues in projects we work on, day to day. These components follow Cerner’s Design Standard, are well-maintained and well-tested, and integrate with webpack-based workflows.'}
+          {'Terra UI offers a set of configurable React components designed to build scalable and modular healthcare-centric applications. The UI framework was created to solve real world issues in projects we work on, day to day.'}
         </Text>
       </Grid.Column>
       <Grid.Column col={2} />

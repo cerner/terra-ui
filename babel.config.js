e26c11fd62dcf56d0c3f3270d4e404ad80ab63be
Matt Henkes <mjhenkes@gmail.com>
2019-07-09 12:58:02 -0500

    remove babel

--- babel.config.js
+++ babel.config.js
@@ -9,7 +9,6 @@ module.exports = (api) => {
 
   const plugins = [
     '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-transform-object-assign',
   ];
 

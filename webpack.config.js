64d87ff5d6a0374d8c3bae082fddbf7ac0846410
Matt Henkes <mjhenkes@gmail.com>
2021-08-31 16:33:48 -0500

    Upgrade to terra-dev-site v7 (#319)
    * terra-dev-site v7 updates
    
    * remove entry points
    
    * latest changes
    
    * remove dialog modal
    
    * whoops, gotta load them src pages in prod too
    
    * update terra-dev-site-branch
    
    * Dev site v7 with terra app v1 and webpack v5
    
    * clean up start
    
    * remove config folder
    
    * consume released package
    
    * remove tgz

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,68 @@
-/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
 const { merge } = require('webpack-merge');
 const {
   TerraDevSite,
-  TerraDevSiteEntrypoints,
-  DirectorySwitcherPlugin,
-  LocalPackageAliasPlugin,
-} = require('terra-dev-site');
-
+} = require('@cerner/terra-dev-site');
 const WebpackConfigTerra = require('@cerner/webpack-config-terra');
-/**
-* Generates the file representing app name configuration.
-*/
-const devSiteConfig = (env = {}, argv = { p: false }) => {
-  const production = argv.p;
 
-  return {
-    entry: TerraDevSiteEntrypoints,
-    plugins: [
-      new TerraDevSite({ env }),
-    ],
-    resolve: {
-      plugins: [
-        new DirectorySwitcherPlugin({
-          shouldSwitch: !production,
-        }),
-        new LocalPackageAliasPlugin(),
+const packageJson = require('./package.json');
+
+const cwd = process.cwd();
+
+const excludes = [
+  '@cerner/terra-application',
+];
+
+const additionalSearchDirectories = Object.keys(packageJson.dependencies).reduce((acc, dependency) => {
+  if ((dependency.startsWith('terra-') || dependency.startsWith('@cerner/terra-')) && !excludes.includes(dependency)) {
+    acc.push(path.join(cwd, 'node_modules', dependency, 'lib', 'terra-dev-site'));
+  }
+  return acc;
+}, []);
+
+const coreConfig = () => ({
+  plugins: [
+    new TerraDevSite({
+      titleConfig: {
+        title: 'Terra',
+      },
+      primaryNavigationItems: [{
+        path: '/home',
+        label: 'Home',
+        contentExtension: 'home',
+      }, {
+        path: '/about',
+        label: 'About',
+        contentExtension: 'about',
+      }, {
+        path: '/application',
+        label: 'Application',
+        contentExtension: 'app',
+      }, {
+        path: '/components',
+        label: 'Components',
+        contentExtension: 'doc',
+      }, {
+        path: '/dev_tools',
+        label: 'Developer Tools',
+        contentExtension: 'tool',
+      }, {
+        path: '/guides',
+        label: 'Guides',
+        contentExtension: 'guide',
+      }],
+      distributionFolder: 'src',
+      additionalSearchDirectories,
+      sideEffectImportFilePaths: [
+        'terra-ui/src/initializeXFC.js',
+        'terra-ui/src/IllustrationGrid.scss',
       ],
-    },
-  };
-};
+    }),
+  ],
+});
 
-const webpackConfig = (env, argv) => (
-  merge(WebpackConfigTerra(env, argv), devSiteConfig(env, argv))
+const mergedConfig = (env, argv) => (
+  merge(WebpackConfigTerra(env, argv), coreConfig())
 );
 
-module.exports = webpackConfig;
+module.exports = mergedConfig;
